be555a13ca275f71ac370458fba20d4a8caed6dc
fanpeichao <fan552426811@outlook.com>
2024-01-04 12:05:18 +0800

    all

--- ruoyi-ui/src/layout/components/Navbar.vue
+++ ruoyi-ui/src/layout/components/Navbar.vue
@@ -102,7 +102,9 @@ export default {
         type: 'warning'
       }).then(() => {
         this.$store.dispatch('LogOut').then(() => {
-          location.href = '/index';
+          // location.href = '/index';
+          this.$router.push('/index')
+
         })
       }).catch(() => {});
     }
